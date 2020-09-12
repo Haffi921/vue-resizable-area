2d5432b3d3852f01c1557cfc66e927b615cf088d
Haffi921 <haffi921@gmail.com>
2020-09-12 02:25:36 +0200

    Bootstrapped rect so it's not grounded to the DOM rect. Instead it's calculated on the fly. Possibly add grounding to the DOM rect in the future at certain infrequent and unobtrusive events.

--- src/components/ResizableArea.vue
+++ src/components/ResizableArea.vue
@@ -10,6 +10,12 @@
 			v-for="(type, corner) in corners" :key="corner"
 			:type="type" :moveFunction="getMove(corner + 'Move')"
 			:width="handleWidth" />
+		left: {{ this.rect.left }}<br>
+		top: {{ this.rect.top }}<br>
+		right: {{ this.rect.right }}<br>
+		bottom: {{ this.rect.bottom }}<br>
+		width: {{ this.rect.width }}<br>
+		height: {{ this.rect.height }}<br>
 	</div>
 </template>
 
@@ -69,6 +75,7 @@ export default {
 			bottomLeft: 'bottom-left',
 		},
 
+		/* Rects */
 		// Self
 		rect: {
 			left: 0,
@@ -78,10 +85,8 @@ export default {
 			width: 0,
 			height: 0,
 		},
-
-		// Rects
-		domRect: {},
-		parent: {},
+		// Parent
+		parentRect: {},
 
 		// Paddings for cursor on handle
 		cursorPadding: 0,
@@ -99,15 +104,12 @@ export default {
 	created() {
 		this.rect.width = this.initWidth;
 		this.rect.height = this.initHeight;
-	},
-	beforeMount() {
+		this.rect.right = this.rect.left + this.rect.width;
+		this.rect.bottom = this.rect.top + this.rect.height;
 	},
 	mounted() {
 		// Initial set up of area
-		this.applyRect();
-
-		// Update rects
-		this.refreshRect();
+		this.updateDOM();
 		this.refreshParent();
 
 		// Update rects on window change
@@ -120,25 +122,22 @@ export default {
 		if (this.grid) this.setUpGrid();
 	},
 	methods: {
-		// Set style methods
-		applyRect() {
+		/* Refresh methods */
+		updateDOM() {
 			this.$el.style.width = `${this.rect.width}px`;
 			this.$el.style.height = `${this.rect.height}px`;
 			this.$el.style.left = `${this.rect.left}px`;
 			this.$el.style.top = `${this.rect.top}px`;
-		},
-
-		// Refresh methods
-		refreshRect() {
-			this.domRect = this.$el.getBoundingClientRect();
+			this.rect.right = this.rect.left + this.rect.width;
+			this.rect.bottom = this.rect.top + this.rect.height;
 		},
 		refreshParent() {
 			const parentPadding = window.getComputedStyle(
 				this.$el.parentNode,
 			).padding.match(/\d+/g).map((p) => parseInt(p, 10));
 
-			this.parent = this.$el.parentNode.getBoundingClientRect();
-			this.parent.padding = {
+			this.parentRect = this.$el.parentNode.getBoundingClientRect();
+			this.parentRect.padding = {
 				top: parentPadding[0 % parentPadding.length],
 				right: parentPadding[1 % parentPadding.length],
 				bottom: parentPadding[2 % parentPadding.length],
@@ -154,12 +153,12 @@ export default {
 			window.addEventListener('resize', this.refreshParent);
 		},
 
-		// Binding method
+		/* Binding method */
 		getMove(moveName) {
 			return this[moveName];
 		},
 
-		// Util methods
+		/* Util methods */
 		xyConditional(xy, x, y) {
 			if (xy === 'x') {
 				return x;
@@ -170,6 +169,7 @@ export default {
 			throw TypeError(`xy is ${xy} but it should be either 'x' or 'y'`);
 		},
 
+		/* Getters */
 		// Get self methods
 		getMinWidthOrHeight(xy) {
 			return this.xyConditional(xy, this.minWidth, this.minHeight);
@@ -239,7 +239,7 @@ export default {
 		// Grid methods
 		setUpGrid() {
 			// Calculate grid
-			[this.gridX, this.gridY] = this.grid.match(/\d+/g);
+			[this.gridX, this.gridY] = this.grid.match(/\d+/g).map(Number);
 			this.gridBuf /= 2;
 
 			// If grid is bigger then minLengths, then minLength is mute
@@ -253,7 +253,12 @@ export default {
 		applyGrid(value, grid) {
 			return Math.round(value / grid) * grid;
 		},
-		applyGridBuf(newValue, currentValue, grid, buf) {
+		applyGridBuf(newValue, currentValue, maxValue, grid, buf) {
+			if (newValue > currentValue
+				&& currentValue + grid > maxValue) {
+				return currentValue;
+			}
+
 			const gridBuf = grid * buf;
 			const valueBuf = newValue < currentValue
 				? newValue + gridBuf
@@ -261,143 +266,133 @@ export default {
 
 			return this.applyGrid(valueBuf, grid);
 		},
-
 		// Transition
 		// removeTransition() {
 		// 	this.$el.style.removeProperty('transition');
 		// },
 
-		// Move methods
+		/* Move methods */
+		// General moves
 		dimensionMove(xy, e) {
-			// Get dimension name
+			// Get position and dimension name
 			const dimension = this.xyConditional(xy, 'width', 'height');
+			// Get left or top side XY position
+			const lt = this.getLeftOrTop(xy);
+			// Get min/max
+			const min = this.getMinWidthOrHeight(xy);
+			const max = this.getMaxWidthOrHeight(xy);
 
 			// Get cursor x or y-position
 			let wh = this.getRelativeCursor(xy, e)
 				// ...relative to left or top-side
-				- this.getLeftOrTop(xy)
+				- lt
 				// Small padding
 				+ this.cursorPadding;
-
-			// max is parent minus rect distance from left/top side
-			const max = this.getParentWH(xy) - this.getLeftOrTop(xy);
-			const min = this.getMinWidthOrHeight(xy);
 
 			// Clamp wh between min and max values
 			wh = clamp(wh, min, max);
 
 			// Apply grid
 			if (this.grid) {
-				console.log(`grid${upper(xy)}`);
+				const grid = `grid${upper(xy)}`;
 				wh = this.applyGridBuf(
-					wh, this.domRect[dimension], this[`grid${upper(xy)}`], this.gridBuf,
+					wh, this.rect[dimension], this.getMaxWidthOrHeight(xy),
+					this[grid], this.gridBuf,
 				);
-				if (this.domRect[dimension] !== wh) {
-					// Apply styles
-					this.$el.style[dimension] = `${wh}px`;
-				}
-			} else {
-				// Apply styles
-				this.$el.style[dimension] = `${wh}px`;
 			}
+
+			this.rect[dimension] = wh;
 		},
 		positionDimensionMove(xy, e) {
 			// Get position and dimension name
 			const position = this.xyConditional(xy, 'left', 'top');
 			const dimension = this.xyConditional(xy, 'width', 'height');
+			// Get right or bottom side XY position
+			const rb = this.getRightOrBottom(xy);
+			// Get min/max
+			const min = 0;
+			const max = rb - this.getMinWidthOrHeight(xy);
 
 			// Get cursor x or y-position relative to parent
 			let lt = this.getRelativeCursor(xy, e)
 				// Small cursor padding
 				- this.cursorPadding;
 
-			// Get right or bottom side XY position
-			const rb = this.getRightOrBottom(xy);
-
-			const min = 0;
-			const max = rb - this.getMinWidthOrHeight(xy);
-
 			// Clamp leftX between min and max
 			lt = clamp(lt, min, max);
 
 			if (this.grid) {
+				const grid = `grid${upper(xy)}`;
 				lt = this.applyGridBuf(
-					lt, this.domRect[position], this[`grid${upper(xy)}`], this.gridBuf,
+					lt, this.rect[position], this.getParentWH(xy),
+					this[grid], this.gridBuf,
 				);
-				if (this.getLeftOrTop(xy) !== lt) {
-					// Apply styles
-					this.$el.style[position] = `${lt}px`;
-					this.$el.style[dimension] = `${rb - lt}px`;
-				}
-			} else {
-				// Apply styles
-				this.$el.style[position] = `${lt}px`;
-				this.$el.style[dimension] = `${rb - lt}px`;
 			}
-		},
 
-		// Side moves
+			this.rect[position] = lt;
+			this.rect[dimension] = rb - lt;
+		},
+		// Sides
 		leftMove(e) {
 			/* Apply changes */
 			this.positionDimensionMove('x', e); // Left
 
 			/* Refresh */
-			this.refreshRect();
+			this.updateDOM();
 		},
 		topMove(e) {
 			/* Apply changes */
 			this.positionDimensionMove('y', e); // Top
 
 			/* Refresh */
-			this.refreshRect();
+			this.updateDOM();
 		},
 		rightMove(e) {
 			/* Apply changes */
 			this.dimensionMove('x', e); // Right
 
 			/* Refresh */
-			this.refreshRect();
+			this.updateDOM();
 		},
 		bottomMove(e) {
 			/* Apply changes */
 			this.dimensionMove('y', e); // Bottom
 
 			/* Refresh */
-			this.refreshRect();
+			this.updateDOM();
 		},
-
-		// Corner moves
+		// Corners
 		topLeftMove(e) {
 			/* Apply changes */
 			this.positionDimensionMove('y', e); // Top
 			this.positionDimensionMove('x', e); // Left
 
 			/* Refresh */
-			this.refreshRect();
+			this.updateDOM();
 		},
 		topRightMove(e) {
 			/* Apply changes */
-			this.positionDimensionMove('y', e);	// Top
-			this.dimensionMove('x', e); 		// Right
+			this.positionDimensionMove('y', e); // Top
+			this.dimensionMove('x', e); // Right
 
 			/* Refresh */
-			this.refreshRect();
+			this.updateDOM();
 		},
 		bottomRightMove(e) {
 			/* Apply changes */
-			this.dimensionMove('y', e);	// Bottom
-			this.dimensionMove('x', e);	// Right
+			this.dimensionMove('y', e); // Bottom
+			this.dimensionMove('x', e); // Right
 
 			/* Refresh */
-			this.refreshRect();
+			this.updateDOM();
 		},
 		bottomLeftMove(e) {
 			/* Apply changes */
-			this.dimensionMove('y', e);			// Bottom
-			this.positionDimensionMove('x', e);	// Left
+			this.dimensionMove('y', e); // Bottom
+			this.positionDimensionMove('x', e); // Left
 
 			/* Refresh */
-			this.refreshRect();
+			this.updateDOM();
 		},
 	},
 };
