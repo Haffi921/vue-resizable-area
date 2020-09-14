673d2983cfaa877138ae2ebf990a90035f63a387
Hafsteinn Ragnarsson <haffi921@gmail.com>
2020-09-14 04:46:51 +0200

    ParentRectMixin added with all parent logic

--- src/components/ResizableArea.vue
+++ src/components/ResizableArea.vue
@@ -27,8 +27,9 @@
 
 <script>
 import { clamp, gte, gt } from 'lodash';
-import HTMLRectMixin from './mixins/HTMLRectMixin';
-import ResizeHandle from './ResizeHandle.vue';
+import HTMLRectMixin from '@/components/mixins/HTMLRectMixin';
+import ParentRectMixin from '@/components/mixins/ParentRectMixin';
+import ResizeHandle from '@/components/ResizeHandle.vue';
 
 const RectPropsMixin = {
 	props: {
@@ -168,9 +169,6 @@ export default {
 			bottomLeft: 'bottom-left',
 		},
 
-		// Parent rect
-		parent: {},
-
 		// Paddings for cursor on handle
 		cursorPadding: 0,
 
@@ -254,27 +252,6 @@ export default {
 			// Get new right and bottom
 			this.setRight(left + width);
 			this.setBottom(top + height);
-		},
-		refreshParent() {
-			const padding = window.getComputedStyle(
-				this.$el.parentNode,
-			).padding.match(/\d+/g).map((p) => parseInt(p, 10));
-
-			this.parent = this.$el.parentNode.getBoundingClientRect();
-			this.parent.padding = {
-				top: padding[0 % padding.length],
-				right: padding[1 % padding.length],
-				bottom: padding[2 % padding.length],
-				left: padding[3 % padding.length],
-			};
-
-			// If padding is 'padding: {top} {left/right} {bottom};'
-			if (padding.length === 3) this.parent.padding.left = this.parent.padding.right;
-
-			if (this.restrictToParent) {
-				this.setMaxWidth(this.getParentW());
-				this.setMaxHeight(this.getParentH());
-			}
 		},
 
 		// Reload rects
@@ -416,27 +393,6 @@ export default {
 				}
 			}
 			throw TypeError(`xy is ${xy} but it should be either 'x' or 'y'`);
-		},
-
-		// Get parent methods
-		getParentWH(xy) {
-			return this.xyConditional(xy, this.getParentW, this.getParentH);
-		},
-		getParentX() {
-			return this.parent.x + this.parent.padding.left;
-		},
-		getParentY() {
-			return this.parent.y + this.parent.padding.top;
-		},
-		getParentW() {
-			const { left, right } = this.parent.padding;
-			const totalXPadding = left + right;
-			return this.parent.width - totalXPadding;
-		},
-		getParentH() {
-			const { top, bottom } = this.parent.padding;
-			const totalYPadding = top + bottom;
-			return this.parent.height - totalYPadding;
 		},
 
 		// Get cursor methods
---
+++ src/components/mixins/ParentRectMixin.js
@@ -0,0 +1,47 @@
+export default {
+	data: () => ({
+		parent: {},
+	}),
+	methods: {
+		refreshParent() {
+			const padding = window.getComputedStyle(
+				this.$el.parentNode,
+			).padding.match(/\d+/g).map((p) => parseInt(p, 10));
+
+			this.parent = this.$el.parentNode.getBoundingClientRect();
+			this.parent.padding = {
+				top: padding[0 % padding.length],
+				right: padding[1 % padding.length],
+				bottom: padding[2 % padding.length],
+				left: padding[3 % padding.length],
+			};
+
+			// If padding is 'padding: {top} {left/right} {bottom};'
+			if (padding.length === 3) this.parent.padding.left = this.parent.padding.right;
+
+			if (this.restrictToParent) {
+				this.setMaxWidth(this.getParentW());
+				this.setMaxHeight(this.getParentH());
+			}
+		},
+		getParentX() {
+			return this.parent.x + this.parent.padding.left;
+		},
+		getParentY() {
+			return this.parent.y + this.parent.padding.top;
+		},
+		getParentW() {
+			const { left, right } = this.parent.padding;
+			const totalXPadding = left + right;
+			return this.parent.width - totalXPadding;
+		},
+		getParentH() {
+			const { top, bottom } = this.parent.padding;
+			const totalYPadding = top + bottom;
+			return this.parent.height - totalYPadding;
+		},
+		getParentWH(xy) {
+			return this.xyConditional(xy, this.getParentW, this.getParentH);
+		},
+	},
+};
